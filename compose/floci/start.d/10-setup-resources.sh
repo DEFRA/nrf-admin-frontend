51cbf5e47c64b0a64fbd5d5d1ada7f06782536ff
Engin Gem <engingem@gmail.com>
2026-05-29 16:36:12 +0100

    fix floci errors

--- compose/floci/start.d/10-setup-resources.sh
+++ compose/floci/start.d/10-setup-resources.sh
@@ -4,4 +4,8 @@
 aws s3 mb s3://data_backups
 
 # SQS queues
-#aws sqs create-queue --queue-name my-queue
+aws sqs create-queue --queue-name cdp-uploader-download-requests
+aws sqs create-queue --queue-name cdp-clamav-results
+aws sqs create-queue --queue-name mock-clamav
+aws sqs create-queue --queue-name cdp-uploader-scan-results-callback.fifo \
+  --attributes FifoQueue=true,ContentBasedDeduplication=true
